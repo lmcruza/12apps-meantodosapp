c2754401dd02443b5cdce3003c9f7f07acecb736
Luis Manuel Cruza <luismanuel.cruza@gmail.com>
2016-12-16 20:24:57 +0100

    Display Todos from MongoDB

--- client/app/components/todos.component.ts
+++ client/app/components/todos.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Todo } from '../Todo';
+import { TodoService } from '../services/todo.service';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 @Component({
     moduleId: module.id,
     selector: 'todos',
     templateUrl: 'todos.component.html'
 })
 export class TodosComponent implements OnInit {
-    constructor() { }
 
-    ngOnInit() { }
+    todos: Todo[];
+    
+    constructor(private _todoService:TodoService) {
+
+    }
+
+    ngOnInit() {
+        this.todos = [];
+        this._todoService.getTodos()
+            .map(res => res.json())
+            .subscribe(todos => this.todos = todos);
+    }
 }
