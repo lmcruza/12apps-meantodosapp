180cdc8dd09fd64c76eadf998a82fbcb19e9b5ed
Luis Manuel Cruza <luismanuel.cruza@gmail.com>
2016-12-16 21:03:24 +0100

    Saving Todos

--- client/app/services/todo.service.ts
+++ client/app/services/todo.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { Todo } from '../Todo';
+
 @Injectable()
 export class TodoService {
 
@@ -11,5 +13,12 @@ export class TodoService {
 
     getTodos() {
         return this._http.get('/api/v1/todos');
+    }
+
+    saveTodo(todo:Todo) {
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this._http.post('/api/v1/todo', JSON.stringify(todo), {headers: headers})
+            .map(res => res.json());
     }
 }
